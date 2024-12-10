2b4a40b07c0f2e8bd8ef88ebdf673edc64c2bff8
ephrim-s <ephrims5@gmai.com>
2024-12-10 12:18:57 -0800

    updated

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-savings = income - expenses
-projected = (savings * 12 + (savings * 12 * 0.05))
-print(f"Your monthly savigns are ${savings}")
-print(f"Projected savings after one year, with interest, is: ${projected}")
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print(f"Your monthly savigns are ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")
