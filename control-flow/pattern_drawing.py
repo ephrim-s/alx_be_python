05068f7740697350df826940e023a760d1d20e45
ephrim-s <ephrims5@gmai.com>
2024-12-17 19:58:13 -0800

    updated

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,10 @@
 number = int(input("Enter the size of the pattern: "))
+x = 0
 
-for x in range(number):
-    print("")
-    for y in range(number):
+while x < number:
+    y = 0
+    while y < number:
         print("*", end="")
+        y += 1
+    print()
+    x += 1
