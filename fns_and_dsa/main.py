2f6e3ed257d4f3d6a14670fbeeebafff98fbf002
ephrim-s <ephrims5@gmail.com>
2025-01-09 20:29:54 +0000

    updated

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,12 +1,5 @@
 from shopping_list_manager import *
 
-def display_menu():
-    print("Shopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
-
 def main():
     shopping_list = []
     while True:
--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,9 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
 def add_item(shopping_list):
     item = input("Enter the item to add: ")
