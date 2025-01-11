0dd7030a72104a9a0ec4574fe25a4bb979ee3e64
ephrim-s <ephrims5@gmail.com>
2025-01-11 22:05:39 +0000

    updated

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -11,7 +11,7 @@ def convert_to_fahrenheit(celsius):
 
 def main():
     try:
-        temperature = float(input("Enter value of temperature: "))
+        temperature = float(input("Enter the temperature to convert: "))
         unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
         if unit == 'F':
             converted = convert_to_celsius(temperature)
