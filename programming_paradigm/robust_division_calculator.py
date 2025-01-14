b2080f4486ed76f37a4b93528131a2a757d8bf33
ephrim-s <ephrims5@gmail.com>
2025-01-14 22:18:19 +0000

    updated

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,5 +1,8 @@
 def safe_divide(numerator, denominator):
     try:
+        if denominator == 0:
+            raise ValueError:
+                print("Error: Cannot divide by zero.")
         return int(numerator)/int(denominator)
     except Exception as e:
         return e
