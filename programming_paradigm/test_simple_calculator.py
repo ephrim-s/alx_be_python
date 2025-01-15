0f83a5aea7616d09c46fbb9c07506f5cfb9bcd11
ephrim-s <ephrims5@gmail.com>
2025-01-15 12:53:43 +0000

    updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,21 +2,25 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-    def test_add(self):
-        result = SimpleCalculator.add(self, 5, 2)
-        self.assertEqual(result, 7)
+    def sefUp(self):
+        self.calc = SimpleCalculator()
+        
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2, 3), 5)
+        self.assertEqual(self.calc.subtract(-1, 1), 0)
     
-    def test_subtract(self):
-        result = SimpleCalculator.subtract(self, 5, 2)
-        self.assertEqual(result, 3)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2, 3), 5)
+        self.assertEqual(self.calc.multiply(-1, 1), 0)
     
-    def test_multiply(self):
-        result = SimpleCalculator.multiply(self, 5, 2)
-        self.assertEqual(result, 10)
-    
-    def test_divide(self):
-        result = SimpleCalculator.divide(self, 5, 2)
-        self.assertEqual(result, 2.5)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(2, 3), 5)
+        self.assertEqual(self.calc.divide(-1, 1), 0)
 
+        
 if __name__ == "__main__":
   unittest.main()
